7fac7578f2c9dfa0278f5897d7a932b986afc4f9
Tabea Ratzke <tabratzke@gmail.com>
2021-01-05 06:42:52 +0100

    add highscore

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor("cornsilk4")
+screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
-speed = 0.07
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -25,36 +24,24 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(speed)
+    time.sleep(0.1)
     snake.move()
 
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        speed -= 0.001
         scoreboard.increase_score()
 
-    # # Detect collision with wall
-    # if snake.head.xcor() > 290 or snake.head.xcor() < -290 or \
-    #         snake.head.ycor() > 290 or snake.head.ycor() < -290:
-    #     game_is_on = False
-    #     scoreboard.game_over()
-
-    # Alternative to normal walls: Infinite Walls
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280:
-        new_x = -(snake.head.xcor())
-        new_y = snake.head.ycor()
-        snake.head.goto(new_x, new_y)
-    elif snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        new_x = snake.head.xcor()
-        new_y = -(snake.head.ycor())
-        snake.head.goto(new_x, new_y)
+    # Detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        scoreboard.reset_scoreboard()
+        snake.reset_snake()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset_scoreboard()
+            snake.reset_snake()
 
 screen.exitonclick()
