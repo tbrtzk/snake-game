b7b1002e274077aed2b0c24b68265d700648c091
Tabea Ratzke <tabratzke@gmail.com>
2021-01-20 05:47:24 +0100

    fix reset_scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -25,8 +25,8 @@ class Scoreboard(Turtle):
             self.high_score = self.score
             with open("data.txt", mode="w") as data:
                 data.write(f"{self.high_score}")
-            self.score = 0
-            self.update_scoreboard()
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
